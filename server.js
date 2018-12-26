d9e6e06ab991245b63a67feacf5032ac3586cb68
urvisha <urvisha@commercepundit.com>
2018-12-26 18:20:13 +0530

    added start and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -59,4 +60,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`connection is up on port ${port}`);
+});
