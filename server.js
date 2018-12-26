b8d7949e692de52a4d10fe5be799759e91274961
urvisha <urvisha@commercepundit.com>
2018-12-26 17:30:47 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (error) => {
+        console.log("Not able to write the log");
+    });
+    next();
+
+});
+
+/*app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear();
+});
+
+hbs.registerHelper('streamIt', (text) => {
+    return text.toUpperCase();
+});
+app.get('/',(req, res) => {
+    //res.send("Hello Express...");
+    /*res.send({
+       name: 'urvisha',
+        likes: [
+            'singing',
+            'dancing'
+        ]
+    });*/
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Welcome to brand new nodejs website.'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    /*res.send("About us page");*/
+    res.render('about.hbs',{
+        pageTitle: 'About us Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Bad request..."
+    });
+});
+
+app.listen(3000);
